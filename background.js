e64da75fa72ea4f9703de146f2997315c79cb412
jaluc <2128029+jaluc@users.noreply.github.com>
2018-08-21 15:00:18 +0200

    Update page action condition
    hostEquals and pathPrefix

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function(){
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions:[new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'pl-aem6.upc.biz'},
+				pageUrl: {hostEquals: 'pl-aem6.upc.biz', pathPrefix: '/cf'}
 			})],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
