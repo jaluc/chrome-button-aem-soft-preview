4b6255b410c12887fc4b4641a88162a389a9ec6d
jaluc <2128029+jaluc@users.noreply.github.com>
2018-08-22 12:10:39 +0200

    Comment background.js

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
+// When the extension is installed or updated …
 chrome.runtime.onInstalled.addListener(function(){
+	// Replace all rules …
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		// With a new rule …
 		chrome.declarativeContent.onPageChanged.addRules([{
+			// That fires when:
 			conditions:[new chrome.declarativeContent.PageStateMatcher({
 				pageUrl: {hostEquals: 'pl-aem6.upc.biz', pathPrefix: '/cf'}
 			})],
+			// And show the extension's page action
 			actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
 	});
