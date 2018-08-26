33e2cd6c0b9f75d81072a88cf0d63f44fb609924
Jakób <qho1397@gmail.com>
2018-08-26 21:05:34 +0200

    Add code comments

--- background.js
+++ background.js
@@ -17,18 +17,22 @@ chrome.runtime.onInstalled.addListener(function(){
 function openPreview(){
 	// Get the currently selected tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		var current = tabs[0]; // The current tab is an object
+		var current = tabs[0];
 		// Extract url of the current tab from the tab object
 		var newUrl = current.url;
+		// define the proper wcm mode parameter
 		var addParam ="?wcmmode=disabled";
+		// use regexp to match cf#/
 		var findCf = /cf#\//;
+		// remove cf#/ part from the URL
 		newUrl = newUrl.replace(findCf, "");
+		// split the url to remove any part starting with ?
 		newUrl = newUrl.split('?');
+		// append the wcm mode param to the remaining URL string 
 		newUrl = newUrl[0]+addParam;
 		// Open a new incognito window containing current tab's URL
 		chrome.windows.create({url: newUrl, incognito: true});
     });
 }
-
 // Set up a click handler to open a new window
 chrome.pageAction.onClicked.addListener(openPreview);
